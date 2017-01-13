49c4c676cab861b3f63f610e16f558d812054884
Daniel Quillen <dquillen@taphere.com>
2017-01-13 09:27:45 -0500

    Adds testing, data structure

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,34 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Dan Quillen on 1/13/17.
+//  Copyright © 2017 Taphere Technology. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 exclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UITextFieldDelegate,
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
-    
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,9 +68,5 @@ class ViewController: UIViewController, UITextFieldDelegate,
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-
 }
 
